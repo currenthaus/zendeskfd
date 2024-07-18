7cd6eb6c53f942eafe795ec28eb638160b4eb4ba
Current Haus <dev@currenthaus.com>
2024-07-18 14:37:28 +0300

    Create zendeskfd.php

---
+++ src/translations/en/zendeskfd.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * zendeskfd plugin for Craft CMS 3.x
+ *
+ * Forward form submissions to zendesk
+ *
+ * @link      https://www.mitcs.com
+ * @copyright Copyright (c) 2022 MatsonIsomTech
+ */
+
+/**
+ * zendeskfd en Translation
+ *
+ * Returns an array with the string to be translated (as passed to `Craft::t('zendeskfd', '...')`) as
+ * the key, and the translation as the value.
+ *
+ * http://www.yiiframework.com/doc-2.0/guide-tutorial-i18n.html
+ *
+ * @author    MatsonIsomTech
+ * @package   Zendeskfd
+ * @since     1.0.0
+ */
+return [
+    'zendeskfd plugin loaded' => 'zendeskfd plugin loaded',
+];
